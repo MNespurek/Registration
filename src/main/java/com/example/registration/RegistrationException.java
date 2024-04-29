9ef8fb928f9054b730b27575fc3da618f9f8565d
michal.nespurek <michal.nespurek@email.cz>
2024-04-29 15:10:23 +0200

    Create class for exceptions

---
+++ src/main/java/com/example/registration/RegistrationException.java
@@ -0,0 +1,11 @@
+package com.example.registration;
+
+public class RegistrationException extends Exception {
+
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+
+
+}
