b2a1829727303120390f351f751d609a15bf7c6d
michal.nespurek <michal.nespurek@email.cz>
2024-04-30 14:29:05 +0200

    methods for getting User from database to put object of User to database.

--- src/main/java/com/example/registration/controller/UserController.java
+++ src/main/java/com/example/registration/controller/UserController.java
@@ -1,17 +1,45 @@
 package com.example.registration.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.registration.RegistrationException;
+import com.example.registration.model.User;
+import com.example.registration.repository.UserRepository;
+import com.example.registration.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 @RequestMapping("api/v1")
 @RestController
 public class UserController {
+    @Autowired
+    UserService user;
+    UserRepository userRepository;
     @PostMapping("user")
-    public String saveUser() {
+    public String saveUser(@RequestBody User user) throws RegistrationException, SQLException {
+        userRepository.saveUserToDatabase(user);
+        return "Uživatel s "+user.getUniqueId()+ " byl úspěšně uložen do databáze.";
+    }
+
+    @GetMapping("/user/{ID}")
+    public String getId(@PathVariable(value = "id")String id) {
+
 
     }
+
+    @GetMapping("/users/{ID}?detail=true")
+    public String getIdFullVersion(@PathVariable(value="id")String id, @RequestParam(value = "detail") Boolean detail) {
+
+    }
+
+    @GetMapping("/users/{ID}")
+    public String getIDFullVersion()
+
 
 
 }
