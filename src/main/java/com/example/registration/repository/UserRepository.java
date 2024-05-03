6ffb0b86d4d816a28edad28282a7404cbd648298
michal.nespurek <michal.nespurek@email.cz>
2024-05-03 14:34:45 +0200

    adding methods for getting user data from database, getting data from file, getting data for editing person

--- src/main/java/com/example/registration/repository/UserRepository.java
+++ src/main/java/com/example/registration/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.registration.repository;
 
 import com.example.registration.RegistrationException;
 import com.example.registration.model.User;
+import com.example.registration.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -10,41 +12,46 @@ import java.util.Set;
 
 @Repository
 public class UserRepository {
+@Autowired
+User user;
 
-    public Statement connectionToDatabaseStatement() throws SQLException, RegistrationException {
+    public ResultSet connectionToDatabaseResultset(String query) throws SQLException, RegistrationException {
         try {
-            Connection connection = DriverManager.getConnection(Settings.DATABASEURL, Settings.USER, Settings.PASSWORD);
-            return connection.createStatement();
+            Connection connection = DriverManager.getConnection(SettingsRepository.DATABASEURL, SettingsRepository.USER, SettingsRepository.PASSWORD);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            return resultSet;
         }catch (SQLException e) {
             throw new RegistrationException("Nebylo nalezeno spojení s databází!" +e.getLocalizedMessage());
 
         }
     }
 
-    public PreparedStatement connectionToDatabasePrepareStatement(String add) throws SQLException, RegistrationException {
+    public PreparedStatement connectionToDatabasePrepareStatement(String query) throws SQLException, RegistrationException {
         try {
-            Connection connection = DriverManager.getConnection(Settings.DATABASEURL, Settings.USER, Settings.PASSWORD);
-            return connection.prepareStatement(add);
+            Connection connection = DriverManager.getConnection(SettingsRepository.DATABASEURL, SettingsRepository.USER, SettingsRepository.PASSWORD);
+            return connection.prepareStatement(query);
         }catch (SQLException e) {
             throw new RegistrationException("Nebylo nalezeno spojení s databází!" +e.getLocalizedMessage());
 
         }
     }
-
 
     public Set<String> setOfIdsFromDatabase() throws SQLException, RegistrationException {
-        Statement statement = connectionToDatabaseStatement();
-        ResultSet selectPersonIdFromUsers = statement.executeQuery(Settings.GETIDSFROMDATABASE);
-        Set<String> setOfIdFromUsers = new HashSet<>();
-        while (selectPersonIdFromUsers.next()) {
-            String personIdFromUsers = String.valueOf(selectPersonIdFromUsers.next());
-            setOfIdFromUsers.add(personIdFromUsers);
+        try (ResultSet resultset = connectionToDatabaseResultset(SettingsRepository.GETIDSFROMDATABASE)) {
+
+            Set<String> setOfIdFromUsers = new HashSet<>();
+            while (resultset.next()) {
+                String personIdFromUsers = resultset.getString(SettingsRepository.PERSONID);
+                setOfIdFromUsers.add(personIdFromUsers);
+            }
+            return setOfIdFromUsers;
         }
-        return setOfIdFromUsers;
     }
 
     public void saveUserToDatabase(User user) throws SQLException, RegistrationException {
-        try(PreparedStatement preparedStatement = connectionToDatabasePrepareStatement(Settings.DATABASEURL)) {
+        try(PreparedStatement preparedStatement = connectionToDatabasePrepareStatement(SettingsRepository.ADDUSERTODATABASE)) {
             ;
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getSurname());
@@ -57,14 +64,56 @@ public class UserRepository {
         }
     }
 
-    public User getUserFromDatabaseBasicVersion(String personId) {
+    public String getUserFromDatabaseByUuidBasicVersion(String uuid) {
+        try(ResultSet resultSet = connectionToDatabaseResultset(SettingsRepository.getUserFromDatabaseByUUID(uuid))) {
+
+                Long id = resultSet.getLong(1);
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+
+        return "Osoba s "+uuid+" má databázové id: "+id+" jméno: "+name+ " a příjmení: " +surname+".";
 
 
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public User getUserFromDatabaseFullVersion(String personId) {
+    public User getUserFromDatabaseForEdit(String uuid) {
+        try(ResultSet resultSet = connectionToDatabaseResultset(SettingsRepository.getUserFromDatabaseByUUID(uuid))) {
+            String name = resultSet.getString(SettingsRepository.NAME);
+            user.setName(name);
+            String surname = resultSet.getString(SettingsRepository.SURNAME);
+            user.setSurname(surname);
+            return user;
 
 
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getUserFromDatabaseFullVersion(String uuid) {
+        try(ResultSet resultSet = connectionToDatabaseResultset(SettingsRepository.getUserFromDatabaseByUUID(uuid))) {
+
+            Long id = resultSet.getLong(1);
+            String name = resultSet.getString(SettingsRepository.NAME);
+            String surname = resultSet.getString(SettingsRepository.SURNAME);
+            String personId = resultSet.getString(SettingsRepository.PERSONID);
+            String uniqueId = resultSet.getString(SettingsRepository.UNIQUEID);
+
+            return "Osoba s "+uuid+" má databázové id: "+id+" jméno: "+name+ " a příjmení: " +surname+" personId: "+personId+ " uuid: "+uniqueId+".";
+
+
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
