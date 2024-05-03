e105bad019413abd52523746572943919207af32
michal.nespurek <michal.nespurek@email.cz>
2024-05-03 14:04:00 +0200

    deleting method static final variable do not need method

--- src/main/java/com/example/registration/config/Settings.java
+++ src/main/java/com/example/registration/config/Settings.java
@@ -2,9 +2,7 @@ package com.example.registration.config;
 
 public class Settings {
 
-    private static String PERSONIDFILE = "resources/dataPersonId.txt";
+    public static final String PERSONIDFILE = "resources/dataPersonId.txt";
 
-    public static String getPersonIdFile() {
-        return PERSONIDFILE;
-    }
+
 }
