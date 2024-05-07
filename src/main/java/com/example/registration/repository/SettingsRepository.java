10d531757edf67aff85494d340f2bcf96f8bb381
michal.nespurek <michal.nespurek@email.cz>
2024-05-07 10:19:04 +0200

    adding methods for editing, deleting user. Get user with different atributes

--- src/main/java/com/example/registration/repository/SettingsRepository.java
+++ src/main/java/com/example/registration/repository/SettingsRepository.java
@@ -1,12 +1,16 @@
 package com.example.registration.repository;
 
-chanpublic class SettingsRepository {
+import com.example.registration.model.User;
+
+public class SettingsRepository {
 
     public static final String DATABASEURL = "jdbc:mysql://localhost:3306/";
 
     public static final String USER = "root";
     public static final String PASSWORD = "Heslo123";
     public static final String ADDUSERTODATABASE = "INSERT INTO USERS (name, surname, personId, uniqueId) values(?, ?, ?, ?)";
+
+    public static final Long ID = 1L;
 
     public static final String PERSONID = "personId";
 
@@ -16,13 +20,34 @@ chanpublic class SettingsRepository {
 
     public static final String UNIQUEID = "uniqueId";
 
-    public static final String GETIDSFROMDATABASE = "SELECT personId FROM users";
+    public static final String GETIDSFROMDATABASE = "SELECT Id FROM users";
 
-    public static String getUserFromDatabaseByUUID(String uuid) {
-        return "SELECT name, surname FROM users WHERE uniqueId = "+uuid+ ";";
+
+    public static String editUserInDatabase(User user) {
+        return "UPDATE users SET name = "+user.getName()+ ", surname = "+user.getSurname()+" WHERE id = "+user.getId()+";";
     }
-    public static String deleteUserFromDatabaseByUUID(String uuid) {
-        return "DELETE * FROM users WHERE uniqueId = "+uuid+ ";";
+
+    public static String deleteUserFromDatabase(String id) {
+        return "DELETE * FROM users WHERE id = "+id+";";
+    }
+
+    public static String getUserFromDatabaseByIdBasicVersion(String id) {
+        return "SELECT name, surname FROM users WHERE uniqueId = "+id+ ";";
+    }
+
+    public static String getUserFromDatabaseByIdFullVersion(String id) {
+        return "SELECT * FROM users WHERE uniqueId = "+id+ ";";
+    }
+    public static String getUsersFromDatabaseBasicVersion() {
+        return "SELECT id, name, surname FROM users";
+    }
+
+    public static String getUsersFromDatabaseFullVersion() { return "SELECT * FROM users;";}
+
+
+
+    public static String deleteUserFromDatabaseByUUID(String id) {
+        return "DELETE * FROM users WHERE uniqueId = "+id+ ";";
     }
 
 
